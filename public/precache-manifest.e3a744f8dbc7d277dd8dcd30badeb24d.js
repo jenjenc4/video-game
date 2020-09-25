4be07b42a5d69d9c02855db472f577c41ac1b513
siddardhavangala <siddardhavangala@gmail.com>
2020-09-25 11:37:54 -0600

    deployment 01

---
+++ public/precache-manifest.e3a744f8dbc7d277dd8dcd30badeb24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be67870ff5acc7bf07533517467f47b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e29fa34371b85268b260",
+    "url": "/static/css/main.a8878e04.chunk.css"
+  },
+  {
+    "revision": "126b3307539c1736acb9",
+    "url": "/static/js/2.9e65a48f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e65a48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29fa34371b85268b260",
+    "url": "/static/js/main.d52e0b92.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
